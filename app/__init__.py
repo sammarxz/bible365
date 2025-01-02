798ed41cf5b00d9e755d630d63ed2701407d94bf
Sam Marxz <sammarxzz@gmail.com>
2025-01-02 12:16:18 -0300

    feat: add application logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
 from app.extensions import db, migrate, jwt
 from app.config import config
+from app.utils.logging import setup_logging
 
 
 def create_app(config_name='development'):
     app = Flask(__name__)
+
+    setup_logging(app)
+
     app.config.from_object(config[config_name])
 
     CORS(app)
